b9ef031aecdc51f44debd7d5d26ec7f64cdd481b
tonymarvins <tonymarvinscrewman@gmail.com>
2026-02-27 15:25:35 +0300

    Update operations.py
    refactoring math logic into operations

--- operations.py
+++ operations.py
@@ -1,1 +1,16 @@
+def add(a: float, b: float) -> float:
+    return a + b
 
+
+def subtract(a: float, b: float) -> float:
+    return a - b
+
+
+def multiply(a: float, b: float) -> float:
+    return a * b
+
+
+def divide(a: float, b: float) -> float:
+    if b == 0:
+        raise ValueError("division by zero")
+    return a / b
