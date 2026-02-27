52a33031d81885368e6237b15b4ad4525c408f60
tonymarvins <tonymarvinscrewman@gmail.com>
2026-02-27 15:30:37 +0300

    Update main.py
    adding menu print functions

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from operations import add, subtract, multiply, divide
 from utils import get_number
 from config import APP_NAME
+
+def menu():
+    print(f"\n{APP_NAME}")
+    print("1 add")
+    print("2 subtract")
+    print("3 multiply")
+    print("4 divide")
+    print("5 exit")
