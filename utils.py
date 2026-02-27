178498e2d62abca55af425a03dc68d66cadee12c
tonymarvins <tonymarvinscrewman@gmail.com>
2026-02-27 15:26:46 +0300

    Update utils.py
    Add numeric reusable input handling

--- utils.py
+++ utils.py
@@ -1,1 +1,6 @@
-
+def get_number(prompt: str) -> float:
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("invalid number, try again")
